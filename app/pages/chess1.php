a44d47eda55203adcbf15a5b190812282df14183
Prince Sanguan <princesanguan44@gmail.com>
2023-11-25 22:18:09 +0800

    adding books to my blog!

--- app/pages/chess1.php
+++ app/pages/chess1.php
@@ -1,18 +1,44 @@
 <?php
     include "./app/pages/includes/header.php";
-  ?>
+?>
 
 <div class="mx-auto col-md-10">
+    <h3 class="mx-2">The Woodpecker Method</h3>
 
-      <h3 class="mx-2">The Woodpecker Method</h3>
+    <!-- Books Dito yun! -->
+    <img class="mb-4 shadow img-fluid border border-dark" id="image" src="<?=ROOT?>/assets/woodpecker/TheWoodpeckerMethod_page-0001.jpg" alt="broken-image">
+    <button onclick="showPrevious()">Previous</button>
+    <button onclick="showNext()">Next</button>
 
-<!------------Books Dito yun!--------------------------------->
+    <script>
+        var images = [];
+        <?php for ($i = 1; $i <= 300; $i++) : ?>
+            images.push("<?=ROOT?>/assets/woodpecker/TheWoodpeckerMethod_page-<?= sprintf('%04d', $i) ?>.jpg");
+        <?php endfor; ?>
 
-<img class="mb-4 shadow img-fluid" src="<?=ROOT?>/assets/images/construction.gif" alt="broken-image">
-  
-<!------------Books Dito yun!--------------------------------->
-  <?php
+        var currentIndex = 0;
+
+        function showImage(index) {
+            document.getElementById("image").src = images[index];
+        }
+
+        function showNext() {
+            currentIndex = (currentIndex + 1) % images.length;
+            showImage(currentIndex);
+        }
+
+        function showPrevious() {
+            currentIndex = (currentIndex - 1 + images.length) % images.length;
+            showImage(currentIndex);
+        }
+
+        // Initial display
+        showImage(currentIndex);
+    </script>
+
+    <!-- Books Dito yun! -->
+</div>
+
+<?php
     include "./app/pages/includes/footer.php";
-  ?>
-<!-----------------Footer--------------------------------->
-
+?>
