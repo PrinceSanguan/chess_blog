54b193b3b542ad40ba438bf477e7f33b2598f61b
Prince Sanguan <princesanguan44@gmail.com>
2023-11-26 10:41:03 +0800

    adding feature to the books

--- app/pages/woodpecker.php
+++ app/pages/woodpecker.php
@@ -2,10 +2,11 @@
     include "./app/pages/includes/header.php";
 ?>
 
+<!------------------------------------------------- Books Dito yun! ------------------------------------------------------------->
+
 <div class="container mt-5 text-center">
     <h3 class="mx-2">The Woodpecker Method</h3>
 
-    <!-- Books Dito yun! -->
     <img class="mb-2 shadow img-fluid border border-dark" id="image" style="max-height: 400px; max-width: 100%;" src="<?=ROOT?>/assets/woodpecker/TheWoodpeckerMethod_page-0001.jpg" alt="broken-image">
     <p id="pageNumber" class="mt-2">Page 1 of 394</p>
     
@@ -17,44 +18,54 @@
     </div>
 
     <script>
-        var images = [];
-        <?php for ($i = 1; $i <= 394; $i++) : ?>
-            images.push("<?=ROOT?>/assets/woodpecker/TheWoodpeckerMethod_page-<?= sprintf('%04d', $i) ?>.jpg");
-        <?php endfor; ?>
+    var images = [];
+    <?php for ($i = 1; $i <= 394; $i++) : ?>
+        images.push("<?=ROOT?>/assets/woodpecker/TheWoodpeckerMethod_page-<?= sprintf('%04d', $i) ?>.jpg");
+    <?php endfor; ?>
 
-        var currentIndex = 0;
+    var currentIndex = 0;
+    var totalImages = images.length;
 
-        function showImage(index) {
-            document.getElementById("image").src = images[index];
-            document.getElementById("pageNumber").innerText = "Page " + (index + 1) + " of " + images.length;
-        }
+    function showImage(index) {
+        document.getElementById("image").src = images[index];
+        document.getElementById("pageNumber").innerText = "Page " + (index + 1) + " of " + totalImages;
 
-        function showNext() {
-            currentIndex = (currentIndex + 1) % images.length;
+        // Disable or enable Previous and Next buttons based on the current index
+        document.getElementById("previousButton").disabled = index === 0;
+        document.getElementById("nextButton").disabled = index === totalImages - 1;
+    }
+
+    function showNext() {
+        if (currentIndex < totalImages - 1) {
+            currentIndex++;
             showImage(currentIndex);
         }
+    }
 
-        function showPrevious() {
-            currentIndex = (currentIndex - 1 + images.length) % images.length;
+    function showPrevious() {
+        if (currentIndex > 0) {
+            currentIndex--;
             showImage(currentIndex);
         }
+    }
 
-        function jumpToPage() {
-            var pageNumber = document.getElementById("jumpToPage").value;
-            if (pageNumber >= 1 && pageNumber <= images.length) {
-                currentIndex = pageNumber - 1;
-                showImage(currentIndex);
-            } else {
-                alert("Invalid page number. Please enter a valid page number.");
-            }
+    function jumpToPage() {
+        var pageNumber = document.getElementById("jumpToPage").value;
+        if (pageNumber >= 1 && pageNumber <= totalImages) {
+            currentIndex = pageNumber - 1;
+            showImage(currentIndex);
+        } else {
+            alert("Invalid page number. Please enter a valid page number.");
         }
+    }
 
-        // Initial display
-        showImage(currentIndex);
-    </script>
+    // Initial display
+    showImage(currentIndex);
+</script>
 
-    <!-- Books Dito yun! -->
 </div>
+
+<!------------------------------------------------- Books Dito yun! ------------------------------------------------------------->
 
 <?php
     include "./app/pages/includes/footer.php";
